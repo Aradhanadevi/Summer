d12cf952940b2cf1b67d3b88b6f1c8af2e30b699
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-05-29 02:06:13 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import AllTask from "./pages/AllTask";
+import ImportantTask from "./pages/ImportantTask";
+import CompletedTask from "./pages/CompletedTask";
+import PendingTask from "./pages/PendingTask";
 
 const App = () => {
   return (
     <div className='bg-gray-900 text-white h-screen p-2'>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home/>} ></Route>
+          <Route exact path="/" element={<Home/>} >
+            <Route index element={<AllTask/>}/>
+            <Route path="/importanttask" element={<ImportantTask/>}/>
+            <Route path="/completedtask" element={<CompletedTask/>}/>
+            <Route path="/pendingtask" element={<PendingTask/>}/>
+          </Route>
         </Routes>
       </Router> 
     </div>
