a750aff9f179cbe95df272f81486a86d4c3d90cf
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-05-30 16:35:38 +0530

    task

--- src/pages/AllTask.jsx
+++ src/pages/AllTask.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Cards from '../components/Home/Cards';
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const AllTask = () => {
   return (
     <div>
-      <Cards/>
+      <div className='w-full flex justify-end px-4 py-2'>
+        <button><IoIosAddCircleOutline className="text-4xl text-gray-400 hover:text-gray-100 transition-all duration-300"/></button>
+      </div>
+      <Cards home={"true"}/>
+      
     </div>
   )
 }
