d87a76f1f3ea48c313b7f50854cb8ae89804945e
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-05-30 18:54:55 +0530

    hi

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='h-[90vh] flex items-center justify-center'>
+        <div className='p-2 w-2/6 rounded bg-gray-800'>
+            <div>SignUp</div>
+        </div>
+    </div>
   )
 }
 
