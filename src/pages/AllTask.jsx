7ed3e721e1d9721dbe75ec5a984ba7aca9f8d064
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-05-30 15:36:51 +0530

    task

--- src/pages/AllTask.jsx
+++ src/pages/AllTask.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Cards from '../components/Home/Cards';
 
 const AllTask = () => {
   return (
-    <div>AllTask</div>
+    <div>
+      <Cards/>
+    </div>
   )
 }
 
