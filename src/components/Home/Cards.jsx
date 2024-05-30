18ebf613f4e537cdde1f9d8e3977f5a2f2af5d6c
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-05-30 16:35:32 +0530

    cards

--- src/components/Home/Cards.jsx
+++ src/components/Home/Cards.jsx
@@ -1,31 +1,34 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { CiHeart } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 import { IoIosAddCircleOutline } from "react-icons/io";
 
-const Cards = () => {
+const Cards = (home) => {
     const data = [
         {
             title:"Dance",
-            desc: "Can't do anything but dance ."
+            desc: "Can't do anything but dance .",
+            status:"Complete",
         },
         {
             title:"Cry",
-            desc: "My favourite hobbie"
+            desc: "My favourite hobbie",
+            status:"Pending",
         },
         {
             title:"Anixiety",
-            desc: "Just my true best friend cause it never leaves me alone."
+            desc: "Just my true best friend cause it never leaves me alone.",
+            status:"Complete",
         },
         {
             title:"Happy",
-            desc: "One day I will be happy with myself."
+            desc: "One day I will be happy with myself.",
+            status:"Pending",
         },
     ];
-    const [ImportantButton, setImportButton] = useState("Pending");
   return (
-    <div className="grid grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-3 gap-4 p-4">
         {data && data.map((items, i) => (
         <div className=" flex flex-col justify-between bg-gray-700 rounded-sm p-4">
             <div className="bg-gray-700 rounded-sm p-4">
@@ -33,7 +36,7 @@ const Cards = () => {
                 <p className="text-gray-300 my-2">{items.desc}</p>
             </div>
             <div className="mt-4 w-full flex items-center">
-                <button className='bg-red-500 p-2 rounded'>Pending</button>
+                <button className={`${items.status === "Pending" ? "bg-red-500" : "bg-green-500"} p-2 rounded`}>{items.status}</button>
                 <div className="text-white p-2 w-3/6 text-xl font-semibold flex justify-around">
                     <button><CiHeart/></button>
                     <button><CiEdit/></button>
@@ -42,10 +45,11 @@ const Cards = () => {
             </div>
         </div>
     ))}
-    <div className="flex flex-col justify-center items-center bg-gray-700 rounded-sm p-4 text-gray-300 hover:scale-105 hover:cursor-pointer transition-all duration-300" >
+    {home === "true" && <div className="flex flex-col justify-center items-center bg-gray-700 rounded-sm p-4 text-gray-300 hover:scale-105 hover:cursor-pointer transition-all duration-300" >
         <IoIosAddCircleOutline className="text-5xl"/>
         <h2 className="text-2xl mt-4 ">Add New Task</h2>
-    </div>
+    </div>}
+    
     </div>
   );
 };
